bb2212ce0931b996a03bbeb15ef5d4eaf2f1167f
Dreahuck <dreahuck@gmail.com>
2024-03-26 18:34:59 +0100

    Holidays

---
+++ sturdy-couscous.Tests/PublicHolidaysTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using sturdy_cousous.Engine;
+
+namespace sturdy_couscous.Tests
+{
+	public class PublicHolidaysTests
+	{
+
+		[Theory]
+		[InlineData(2024,01,01, false)] //Jour de l'an 2024
+        [InlineData(2024, 04, 01, false)] //Lundi de Pâque 2024
+        [InlineData(2023, 04, 01, true)] //PAS le Lundi de Pâque
+        [InlineData(2024, 05, 01, false)] //1er Mai 2024
+        [InlineData(2024, 05, 08, false)] //8 Mai 2024
+        [InlineData(2024, 05, 09, false)] //Ascension 2024
+        [InlineData(2024, 05, 20, false)] //Pentecôte 2024
+        [InlineData(2024, 07, 14, false)] //Fête nationale 2024
+        [InlineData(2024, 08, 15, false)] //Assomption 2024
+        [InlineData(2024, 11, 01, false)] //La Toussaint 2024
+        [InlineData(2024, 11, 11, false)] //Armistice 2024
+        [InlineData(2024, 12, 25, false)] //Noël 2024
+        public void ShouldReturnFalseIfPublicHoliday(int year, int month, int day, bool result)
+		{
+			var date = new DateTime(year, month, day);
+			Assert.Equal(result, PublicHolidays.IsNotPublicHolidays(date));
+		}
+	}
+}
+
---
+++ sturdy-cousous.Engine/PublicHolidays.cs
@@ -0,0 +1,84 @@
+﻿using System;
+namespace sturdy_cousous.Engine;
+
+public static class PublicHolidays
+{
+    public static bool IsNotPublicHolidays(DateTime date)
+    {
+        var easterDay = EasterDay(date.Year);
+        return !IsNewYear(date)
+            && !IsEasterMonday(easterDay, date)
+            && !IsLabourDay(date)
+            && !IsArmisticeMay(date)
+            && !IsAscension(easterDay, date)
+            && !IsPentecote(easterDay, date)
+            && !IsBastilleDay(date)
+            && !IsAssemption(date)
+            && !IsSaintsDay(date)
+            && !IsArmisticeNovember(date)
+            && !IsChristmasDay(date);
+    }
+    private static DateTime EasterDay(int year)
+    {
+        int a = year % 19;
+        int b = year / 100;
+        int c = year % 100;
+        int d = b / 4;
+        int e = b % 4;
+        int f = (b + 8) / 25;
+        int g = (b - f + 1) / 3;
+        int h = (19 * a + b - d - g + 15) % 30;
+        int i = c / 4;
+        int k = c % 4;
+        int l = (32 + 2 * e + 2 * i - h - k) % 7;
+        int m = (a + 11 * h + 22 * l) / 451;
+        int month = (h + l - 7 * m + 114) / 31;
+        int day = ((h + l - 7 * m + 114) % 31) + 1;
+        return new(year, month, day);
+    }
+
+    private static bool IsNewYear(DateTime date)
+    {
+        return date.Month == 1 && date.Day == 1;
+    }
+    private static bool IsEasterMonday(DateTime easterDay, DateTime date)
+    {
+        return date.Date == easterDay.AddDays(1).Date;
+    }
+    private static bool IsLabourDay(DateTime date)
+    {
+        return date.Month == 5 && date.Day == 1;
+    }
+    private static bool IsArmisticeMay(DateTime date)
+    {
+        return date.Month == 5 && date.Day == 8;
+    }
+    private static bool IsAscension(DateTime easterDay, DateTime date)
+    {
+        return date.Date == easterDay.AddDays(39).Date;
+    }
+    private static bool IsPentecote(DateTime easterDay, DateTime date)
+    {
+        return date.Date == easterDay.AddDays(50).Date;
+    }
+    private static bool IsBastilleDay(DateTime date)
+    {
+        return date.Month == 7 && date.Day == 14;
+    }
+    private static bool IsAssemption(DateTime date)
+    {
+        return date.Month == 8 && date.Day == 15;
+    }
+    private static bool IsSaintsDay(DateTime date)
+    {
+        return date.Month == 11 && date.Day == 1;
+    }
+    private static bool IsArmisticeNovember(DateTime date)
+    {
+        return date.Month == 11 && date.Day == 11;
+    }
+    private static bool IsChristmasDay(DateTime date)
+    {
+        return date.Month == 12 && date.Day == 25;
+    }
+}
